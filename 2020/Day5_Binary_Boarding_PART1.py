128089ee7d2e22d571227320b9bd62517c792067
Marcos Díaz <marcos.dmartin@gmail.com>
2020-12-11 23:07:49 +0100

    Added Day 5
    Added Day 5 to the repo  (part 1 & 2)

---
+++ 2020/Day5_Binary_Boarding_PART1.py
@@ -0,0 +1,41 @@
+# Reading input file
+f = open("e:/Users/Marcos Diaz/Software/GitHub/AdventCode/2020/inputs/Day5_input.txt", "r")
+
+identifierList=[]
+
+#Function to identify the row on the plane given a boarding pass
+def boardingPassRowChecker(boardingPass):
+    minrow=0
+    maxrow=127
+    for i in range(len(boardingPass)-4):
+        if boardingPass[i]=='F':
+            maxrow=(maxrow+minrow)//2
+        else:
+            minrow=(minrow+maxrow+1)//2
+    if boardingPass[6]=='B':
+        return maxrow
+    else:
+        return minrow
+
+#Function to identify the column on the plane given a boarding pass
+def boardingPassColumnChecker(boardingPass):
+    mincol=0
+    maxcol=7
+    for i in range(7,len(boardingPass)):
+        if boardingPass[i]=='L':
+            maxcol=(maxcol+mincol)//2
+        else:
+            mincol=(mincol+maxcol+1)//2
+    if boardingPass[9]=='R':
+        return maxcol
+    else:
+        return mincol
+
+for i in f.readlines():
+    identifier=boardingPassRowChecker(i)*8+boardingPassColumnChecker(i)
+    identifierList.append(identifier)
+
+print("Maximum identifier is: ",max(identifierList))
+
+
+
